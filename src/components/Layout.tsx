09d5169675e2f9ecaa2fbebc91799bfa291e553e
prasunet <162030269+prasunet@users.noreply.github.com>
2025-04-07 15:46:24 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,9 +9,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
-      <header style={{ padding: '1.5rem', borderBottom: '1px solid #ccc' }}>
-        <h2>Find My Career</h2>
-      </header>
+      
       <main>{children}</main>
       <footer style={{ padding: '1.5rem', borderTop: '1px solid #ccc', marginTop: '2rem', textAlign: 'center' }}>
         <small>© 2025 Find My Career | Built with ❤️ by Prasunet</small>
